e5b47c6b246bb3e2bbcd5d31730e631978c0f8b1
Paul Rodriguez <paul@prdesign.work>
2018-04-20 13:27:40 -0600

    buttons added

--- public/myapp.js
+++ public/myapp.js
@@ -2,13 +2,16 @@ $( document ).ready(function() {
 
     $( "a" ).click(function( event ) {
 
-        alert( "Thanks for visiting!" );
+        alert( 'Thanks for visiting!' );
 
     });
 
 });
-this.template = '<div id="salutation"><a href="">Test</a></div>';
+this.template = '<div id="salutation"><a href="#">#</a></div>';
 document.body.innerHTML += this.template;
 
 this.template = '<div id="fullname"></div>';
+document.body.innerHTML += this.template;
+
+this.template = '';
 document.body.innerHTML += this.template;
